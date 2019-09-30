184594b4109638d66879bee3624267fafdecd8fb
Alice <alice@rocketmakers.com>
2019-09-30 21:34:40 +0100

    set up basics

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,12 @@ function Home() {
     <div className="shell">
 
       <div className="nav">
-        <a>home</a>
-        <a href="tel:07766197840" className="right">phone</a>
-        <a href="mailto:paul@twicebakedandloaded.com" className="right">mail</a>
+        <a href="#home" className="home">home</a>
+        {/* <p className="nav-title">Twice baked and loaded</p> */}
+        <a href="tel:07766197840" className="right phone">phone</a>
+        <a href="mailto:paul@twicebakedandloaded.com" className="right mail">mail</a>
       </div>
-
-      <div className="header">
+      <div className="header" id="home">
         <div className="banner1">
           <span>Welcome to</span>
         </div>
@@ -28,20 +28,30 @@ function Home() {
               circle-up
           </a>
           </div>
-
         </div>
+
+        <div className="description-slip">
+          <div>
+            <h4>Twice Baked and Loaded Potato Company are based in Holt, Wiltshire. They serve delicious potatoes at festivals, street markets and are available for private parties or weddings.</h4>
+            <p>Just contact Paul on <a>07766197840</a>or email <a>paul@twicebakedandloaded.com</a></p>
+          </div>
+        </div>
+
         <div className="dark-slice">
-          <div className="slice-text">
-            <h3>
-              One Potato, Two Potato, Three Potato... Paul
-        </h3>
-            <p>
-              Meet Paul McNeil, the man behind the stand.
-        </p>
+          <div className="dark-slice-content">
+            <div className="slice-text">
+              <h3 className="slice-h3">
+                One Potato, Two Potato, Three Potato... <span className="paul">Paul</span>
+            </h3>
+              <p className="slice-p">
+                Meet Paul McNeil, the man behind the stand. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiu ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div>
+              <img className="slice-img" src={require("./assets/images/paul.png")} />
+            </div>
           </div>
-          <div className="slice-img">
-            <img src={require("./assets/images/paul.png")} />
-          </div>
+
 
         </div>
       </div>
