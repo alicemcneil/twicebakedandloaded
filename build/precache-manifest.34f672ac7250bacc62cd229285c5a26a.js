92cb607cd5ad78d9d39ac741dda053875e3b36ed
Alice <alice@rocketmakers.com>
2019-10-04 15:03:54 +0100

    stopped ignoring my build folers

---
+++ build/precache-manifest.34f672ac7250bacc62cd229285c5a26a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e99bdf17824bb8afdd4b4fbeaafaa0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "691743c08096cfebd633",
+    "url": "./static/css/main.98f82c68.chunk.css"
+  },
+  {
+    "revision": "805f39a50094197bd72c",
+    "url": "./static/js/2.8e1394ff.chunk.js"
+  },
+  {
+    "revision": "691743c08096cfebd633",
+    "url": "./static/js/main.ac34d368.chunk.js"
+  },
+  {
+    "revision": "e0680199fb92ef3b913f",
+    "url": "./static/js/runtime-main.06014d18.js"
+  },
+  {
+    "revision": "e261516512d6dc536b9d81f81c3ee1d9",
+    "url": "./static/media/IcoMoon-Ultimate.e2615165.ttf"
+  },
+  {
+    "revision": "3aa02cd2710a61eb63855ee4ca8c2abc",
+    "url": "./static/media/OldStandardTT-Bold.3aa02cd2.ttf"
+  },
+  {
+    "revision": "a8e3a076fda5312e878c6ef9ebbab997",
+    "url": "./static/media/OldStandardTT-Regular.a8e3a076.ttf"
+  },
+  {
+    "revision": "766db6981a8f279ef14dbdc1bc8b85b6",
+    "url": "./static/media/WorkSans-Bold.766db698.ttf"
+  },
+  {
+    "revision": "740b8e9adcb998d4f65b3b18251fb5e2",
+    "url": "./static/media/WorkSans-Regular.740b8e9a.ttf"
+  },
+  {
+    "revision": "5d88023486ee662247c1b3e7a04fda08",
+    "url": "./static/media/insta1.5d880234.png"
+  },
+  {
+    "revision": "474e36aac17676a3f67e7dd2cdaab26e",
+    "url": "./static/media/insta2.474e36aa.png"
+  },
+  {
+    "revision": "ca00cb5c84556ecb6b4deb185e4ba864",
+    "url": "./static/media/insta3.ca00cb5c.png"
+  },
+  {
+    "revision": "7d0f6f08f123f9d50512476623c3fc45",
+    "url": "./static/media/insta4.7d0f6f08.png"
+  },
+  {
+    "revision": "6bb03592fc2e5ca1b0c8e27c824eac88",
+    "url": "./static/media/insta5.6bb03592.png"
+  },
+  {
+    "revision": "29c2803fa6e7f7601c0e8a801937a9ff",
+    "url": "./static/media/insta6.29c2803f.png"
+  },
+  {
+    "revision": "af3a1666694f1c1a98b3a7acae189f9a",
+    "url": "./static/media/insta7.af3a1666.png"
+  },
+  {
+    "revision": "599b7b45ed380c1c9cbce0fbe8db5dfe",
+    "url": "./static/media/just-potatoes.599b7b45.png"
+  },
+  {
+    "revision": "46e35cbd2415402434bf2be4f5594b0b",
+    "url": "./static/media/paul.46e35cbd.png"
+  },
+  {
+    "revision": "b7640cd198793ec805cb5a91447bd319",
+    "url": "./static/media/point-at-potato.b7640cd1.png"
+  },
+  {
+    "revision": "cf83f749ae052083d04da2ca56cc6295",
+    "url": "./static/media/potato-family.cf83f749.png"
+  },
+  {
+    "revision": "41c522be624eed72c3f614f69fdeda1b",
+    "url": "./static/media/potato-paul.41c522be.png"
+  },
+  {
+    "revision": "d5872e283f1172bd281e3401bc938d29",
+    "url": "./static/media/sustainabilty.d5872e28.png"
+  },
+  {
+    "revision": "485e5cbd27cb63f5f91466bf4bf4c0f9",
+    "url": "./static/media/why-potatoes.485e5cbd.png"
+  }
+]);
